4793761433f7a96218753c0e9422faaeab5d85b5
SaifBhuiya <bhuiyasaif@gmail.com>
2024-08-18 12:21:20 +0600

    Asteroid Spawn

---
+++ Scripts/Simulation/asteroid_spawner.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var asteroid = preload("res://Scenes/Prefabs/asteroid.tscn")
+@onready var timer: Timer = $Timer
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+
+
+func _on_timer_timeout() -> void:
+	var item = asteroid.instantiate()
+	item.global_position = global_position
+	timer.start()
+	get_parent().add_child(item)
+	#print("asteroid spawned")
